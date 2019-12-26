9773ee0b48d11eb676ee00e3575e8e427ac5d825
Saarthak Sharma <sharma.saarthak@gmail.com>
2019-12-26 11:45:21 -0500

    basic frontend

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    
+});
+
+app.get("/picture", (req, res)=>{
+
+});
+
+app.get("/picture/:id", (req, res)=>{
+
+});
+
+app.post("/picture", (req, res)=>{
+
+});
+
+
